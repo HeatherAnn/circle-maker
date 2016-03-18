ff45c76c2cda852d22c5ee898375bf01b1021e79
HeatherAnn <heather.bisaillion@gmail.com>
2016-03-18 10:02:48 -0400

    Adding css and working on js.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $body = $('body');
+
+
+$('html').on('keydown', function (e) {
+  var $name = $('<div>');
+  var newLeft = Math.floor(Math.random) + (1000 - 0)) + 0;
+  var newTop = Math.floor(Math.random) + (1000 - 0)) + 0;
+
+  $name.addClass('ball');
+  $body.append('$name');
+  $name.css('left', newLeft);
+  $name.css('top', newTop);
+});
